3864b6c6e8ac67b64552c02c94f62a5443364a3e
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-03 13:48:40 +0200

    create class asciugatrice extend machine

---
+++ csharp-lavanderia/Asciugatrice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lavanderia
+{
+    internal class Asciugatrice : Macchina
+    {
+        private List<Programma> programmi;
+        private bool inUso;
+
+        public List<Programma> Programmi { get; set; }
+        public bool InUso { get; set; }
+
+        public Asciugatrice(List<Programma> programmi,bool inUso) : base(programmi, inUso)
+        {
+            Programmi = programmi;
+            InUso = inUso;
+        }
+    }
+}
