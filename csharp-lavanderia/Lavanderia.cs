9f6b1f7a71594dc17e7accdb3e3849be5ab66409
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-04 00:39:44 +0200

    refactored 1 and 2 of menu

---
+++ csharp-lavanderia/Lavanderia.cs
@@ -0,0 +1,78 @@
+﻿using csharp_lavanderia;
+
+internal class Lavanderia
+{
+    public string Name { get; set; }
+    public List<Asciugatrice> Asciugatrici { get; set; }
+    public List<Lavatrice> Lavatrici { get; set; }
+
+    List<Macchina> macchine = new List<Macchina>
+        {
+            new Lavatrice(1),
+            new Lavatrice(2),
+            new Lavatrice(3),
+            new Lavatrice(4),
+            new Lavatrice(5),
+            new Asciugatrice(1),
+            new Asciugatrice(2),
+            new Asciugatrice(3),
+            new Asciugatrice(4),
+            new Asciugatrice(5),
+    };
+
+    public Lavanderia()
+    {
+        Lavatrici = new List<Lavatrice>();
+        Asciugatrici = new List<Asciugatrice>();
+    }
+
+    public List<Lavatrice> OttieniLavatrici()
+    {
+        foreach (Macchina macchina in macchine)
+        {
+            if (macchina is Lavatrice)
+            {
+                Lavatrici.Add((Lavatrice)macchina);
+            }
+        }
+        return Lavatrici;
+    }
+
+    public List<Asciugatrice> OttieniAsciugatrici()
+    {
+        foreach (Macchina macchina in macchine)
+        {
+            if (macchina is Asciugatrice)
+            {
+                Asciugatrici.Add((Asciugatrice)macchina);
+            }
+        }
+        return Asciugatrici;
+    }
+    public Lavatrice UsaLavatrice()
+    {
+        foreach (Lavatrice lavatrice in Lavatrici)
+        {
+            if (lavatrice.InUso == false)
+            {
+                lavatrice.InUso = true;
+                lavatrice.Avvia();
+                return lavatrice;
+            }
+        }
+        return null;
+    }
+    public Asciugatrice UsaAsciugatrice()
+    {
+        foreach(Asciugatrice asciugatrice in Asciugatrici)
+        {
+            if(asciugatrice.InUso == false)
+            {
+                asciugatrice.InUso = true;
+                asciugatrice.Avvia();
+                return asciugatrice;
+            }
+        }
+        return null;
+    }
+}
--- csharp-lavanderia/Program.cs
+++ csharp-lavanderia/Program.cs
@@ -6,105 +6,33 @@
         public const string Spacer = "@-----------------------------------------------@";
         static void Main(string[] args)
         {
-            
-            List<Macchina> macchine = new List<Macchina>
-            {
-                new Lavatrice(1),
-                new Lavatrice(2),
-                new Lavatrice(3),
-                new Lavatrice(4),
-                new Lavatrice(5),
-                new Asciugatrice(1),
-                new Asciugatrice(2),
-                new Asciugatrice(3),
-                new Asciugatrice(4),
-                new Asciugatrice(5),
-        };
             Console.WriteLine("Benvenuto nella lavenderia");
-            Console.WriteLine("Cosa vuoi usare?");
-            Console.WriteLine("[1]Lavatrice [2]Asciugatrice");
+            Console.WriteLine("Scegli un opzione?");
+            Console.WriteLine("[1]Usa una Lavatrice");
+            Console.WriteLine("[2]Usa un' Asciugatrice");
+            Console.WriteLine("[3]Ottieni lo stato di tutte le lavatrici");
+            Console.WriteLine("[4]Ottieni lo stato di tutte le asciugatrici");
             int scelta = Convert.ToInt32(Console.ReadLine());
-            Macchina macchinaScelta = null;
-            if(scelta == 1)
-            {
-                foreach (Macchina macchina in macchine)
-                {
-                    if (macchina.InUso == false && macchina is Lavatrice)
-                    {
-                        //prendiamo la macchina libera
-                        macchinaScelta = macchina;
-                        macchina.Avvia();             
-                        Console.WriteLine("Scegli il programma");
-                        break;
 
-                    }
-                    else
+            Lavanderia sharpLavanderia = new Lavanderia();
+            sharpLavanderia.OttieniLavatrici();
+            sharpLavanderia.OttieniAsciugatrici();
+            switch (scelta)
+            {
+                case 1:
+                    if (sharpLavanderia.UsaLavatrice() == null)
                     {
                         Console.WriteLine("Nessuna lavatrice libera");
-                        break;
-                    }
-                }
-        
-            }
-            if (scelta == 2)
-            {
-                foreach (Macchina macchina in macchine)
-                {
-                    if (macchina.InUso == false && macchina is Asciugatrice)
+                    };
+                    break;
+                case 2:
+                    if(sharpLavanderia.UsaAsciugatrice() == null)
                     {
-                        macchinaScelta = macchina;
-                        macchina.Avvia();
-                        Console.WriteLine("Scegli il programma");
-                        break;
-                    }
-                }
-
-                if (macchinaScelta == null)
-                {
-                    Console.WriteLine("Nessuna asciugatrice libera");
-                    return;
-                }
+                        Console.WriteLine("Nessuna asciugatrice libera");
+                    };
+                    break;
             }
-            if (macchinaScelta is Lavatrice && macchinaScelta != null)
-            {
-                Lavatrice lavatriceScelta = (Lavatrice)macchinaScelta;
-                Console.WriteLine("[1] Rinfrescante");
-                Console.WriteLine("[2] Rinnovante");
-                Console.WriteLine("[3] Sgrassante");
-                scelta = Convert.ToInt32(Console.ReadLine());
-                switch (scelta)
-                {
-                    case 1:
-                        lavatriceScelta.Rinfrescante();
-                        break;
-                    case 2:
-                        lavatriceScelta.Rinnovante();
 
-                        break;
-                    case 3:
-                        lavatriceScelta.Sgrassante();
-
-                        break;
-                }
-            }
-            if(macchinaScelta is Asciugatrice && macchinaScelta != null)
-            {
-                Asciugatrice asciugatriceScelta = (Asciugatrice)macchinaScelta;
-                Console.WriteLine("[1]Rapido");
-                Console.WriteLine("[2]Intenso");
-                scelta = Convert.ToInt32(Console.ReadLine());
-                switch(scelta)
-                {
-                    case 1:
-                        asciugatriceScelta.Rapido();
-
-                        break;
-                    case 2:
-                        asciugatriceScelta.Intenso();
-                        break;
-                }
-            }
-            Console.WriteLine(macchinaScelta.VediGuadagno());
         }
     }
 }
