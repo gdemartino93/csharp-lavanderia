90b82e020464137fdf80a1d6f4917c376ffbfda6
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-03 13:31:11 +0200

    create class machine

---
+++ csharp-lavanderia/Macchina.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lavanderia
+{
+    abstract class Macchina
+    {
+        private List<Programma> programmi;
+        public List<Programma> Programmi { get; set; }
+
+        public Macchina(List<Programma> programmi)
+        {
+            Programmi = programmi;
+        }
+    }
+}
--- csharp-lavanderia/Program.cs
+++ csharp-lavanderia/Program.cs
@@ -2,9 +2,11 @@
 {
     internal class Program
     {
+        public const double gettone = 0.50;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
         }
     }
 }
